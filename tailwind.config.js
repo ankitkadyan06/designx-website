2adc65268d8131e0c8e52e6ccb533a42dbf1da7b
Ankit Kadyan <ankit.kadyan@designx.in>
2023-11-21 09:47:09 +0530

    Create folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'poppins': [ 'Poppins', 'sans-serif'],
+      'montserrat': [ 'Montserrat', 'sans-serif'],
+      'ubuntu': ['Ubuntu', 'sans-serif'],
+      'inter': ['inter', 'sans-serif'],
+      'robotoSlab': ['Roboto Slab', 'serif']
+    }
+  },
+  plugins: [],
+});
