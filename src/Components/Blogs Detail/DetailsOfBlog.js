3d508ba97ea8f87236c4b02a69767b843ef9bc28
ankitkadyan06 <ankit.kadyan@designx.in>
2024-01-10 11:21:07 +0530

    blog changes

--- src/Components/Blogs Detail/DetailsOfBlog.js
+++ src/Components/Blogs Detail/DetailsOfBlog.js
@@ -2,50 +2,50 @@ import React from 'react'
 
 const DetailsOfBlog = () => {
   return (
-    <div className='ml-[50px] lg:ml-[100px] mt-[52px] mr-[80px] pb-[50px]'>
+    <div className='mx-[30px] md:mx-[50px] xl:mx-[80px] mt-[52px] pb-[50px]'>
       {/* <p className='text-[#FCFCFC] text-[28px] font-poppins'>
       Unveil a plethora of opportunities with IIOT solutions.
       </p> */}
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>In the digital age, the manufacturing industry is shifting towards automation to transform itself. In this race of transformation, the industrial internet of things can bring many opportunities for manufacturing firms. Yes, from process automation to supply chain management to industrial asset tracking, IIOT can assist businesses in using valuable real-time data for their success! With IIOT, one can easily find problems, detect errors, and banish inefficiencies. This step towards industry 4.0 will help us reach the maximum benefits. Let us learn more about IIOT and how we can empower you with these solutions.</p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>Presently, DFOS measures and analyses data in three different ways: the transfer of data from paper to DFOS, machines to DFOS, you to DFOS.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins'>In the digital age, the manufacturing industry is shifting towards automation to transform itself. In this race of transformation, the industrial internet of things can bring many opportunities for manufacturing firms. Yes, from process automation to supply chain management to industrial asset tracking, IIOT can assist businesses in using valuable real-time data for their success! With IIOT, one can easily find problems, detect errors, and banish inefficiencies. This step towards industry 4.0 will help us reach the maximum benefits. Let us learn more about IIOT and how we can empower you with these solutions.</p>
+      <p className='text-[#C8C8C8] text-[18px] font-poppins'>Presently, DFOS measures and analyses data in three different ways: the transfer of data from paper to DFOS, machines to DFOS, you to DFOS.</p>
 
-      <p className='text-[#FCFCFC] text-[28px] mt-[40px] font-poppins'>
+      <p className='text-[#FCFCFC] text-[20px] md:text-[24px] lg:text-[28px] mt-[40px] font-poppins'>
       What is Industrial Internet of Things?
       </p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>In this race to switch to an automated version, the manufacturing industry is seeking help from the IOT (Internet of Things). IOT interconnects a variety of physical objects and technologies to ensure seamless communication. IIOT is an extended version of IOT. The connected devices on the IIOT network can transfer data flawlessly without any human-to-human or human-to-computer interactions. In simple terms, it connects intelligent devices that collect, analyze, and interpret data to help businesses leverage the benefits.</p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>IIOT is helping businesses embrace the advantages of smart solutions and digital tools. But how can it help manufacturing industry and shop floors? Let us find out.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins'>In this race to switch to an automated version, the manufacturing industry is seeking help from the IOT (Internet of Things). IOT interconnects a variety of physical objects and technologies to ensure seamless communication. IIOT is an extended version of IOT. The connected devices on the IIOT network can transfer data flawlessly without any human-to-human or human-to-computer interactions. In simple terms, it connects intelligent devices that collect, analyze, and interpret data to help businesses leverage the benefits.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins'>IIOT is helping businesses embrace the advantages of smart solutions and digital tools. But how can it help manufacturing industry and shop floors? Let us find out.</p>
       
-      <p className='text-[#FCFCFC] text-[28px] mt-[40px] font-poppins'>
+      <p className='text-[#FCFCFC] text-[20px] md:text-[24px] lg:text-[28px] mt-[40px] font-poppins'>
       Data Accessibility
       </p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>By using the IIOT solution, one can have complete access to any data from a specific centre location. It will help manufacturing hubs gain more remote control over inventory and stocks. Additionally, it promotes the process of monitoring and securing assets. So now you can have great control over resources and see them from a distance.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins'>By using the IIOT solution, one can have complete access to any data from a specific centre location. It will help manufacturing hubs gain more remote control over inventory and stocks. Additionally, it promotes the process of monitoring and securing assets. So now you can have great control over resources and see them from a distance.</p>
 
-      <p className='text-[#FCFCFC] text-[28px] mt-[40px]'>
+      <p className='text-[#FCFCFC] text-[20px] md:text-[24px] lg:text-[28px] mt-[40px]'>
       Market Know-how
       </p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>Knowledge about market data is highly important for the manufacturing industry, and with IIOT, one can easily generate actionable insights from effective data mining. This data can help shop floors with better operational quality management, understanding output patterns, and so forth. Hence, market forecasting can help businesses achieve success.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins text-justify'>Knowledge about market data is highly important for the manufacturing industry, and with IIOT, one can easily generate actionable insights from effective data mining. This data can help shop floors with better operational quality management, understanding output patterns, and so forth. Hence, market forecasting can help businesses achieve success.</p>
 
-      <p className='text-[#FCFCFC] text-[28px] mt-[40px] font-poppins'>
+      <p className='text-[#FCFCFC] text-[20px] md:text-[24px] lg:text-[28px] mt-[40px] font-poppins'>
       Productive Manufacturing
       </p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>Real-time data can help increase the productivity of manufacturing units. The end product is affected by economic conditions, the process used, quality decisions, and many other factors. These captured analytics can help in making timely decisions to eliminate wastage and ensure the production is done as per schedule & as per the plan. Hence, IIOT can help in productive manufacturing.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins text-justify'>Real-time data can help increase the productivity of manufacturing units. The end product is affected by economic conditions, the process used, quality decisions, and many other factors. These captured analytics can help in making timely decisions to eliminate wastage and ensure the production is done as per schedule & as per the plan. Hence, IIOT can help in productive manufacturing.</p>
 
-      <p className='text-[#FCFCFC] text-[28px] mt-[40px] font-poppins'>
+      <p className='text-[#FCFCFC] text-[20px] md:text-[24px] lg:text-[28px] mt-[40px] font-poppins'>
       Better Equipment Utilization
       </p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>Utilizing machinery to its maximum output is a difficult task for many manufacturing units. However, with IIOT machine management solutions, you can gain equipment effectiveness, and productivity. So, your organization’s profitability will automatically increase.</p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify font-medium'>So, these are the benefits which manufacturing firms can enjoy after choosing IIOT solutions. Now, let us understand how DFOS empower you with IIOT solutions & how we are helping manufacturing units to go global with automation.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins text-justify'>Utilizing machinery to its maximum output is a difficult task for many manufacturing units. However, with IIOT machine management solutions, you can gain equipment effectiveness, and productivity. So, your organization’s profitability will automatically increase.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins text-justify font-medium'>So, these are the benefits which manufacturing firms can enjoy after choosing IIOT solutions. Now, let us understand how DFOS empower you with IIOT solutions & how we are helping manufacturing units to go global with automation.</p>
 
-      <p className='text-[#FCFCFC] text-[28px] mt-[40px] font-poppins'>
+      <p className='text-[#FCFCFC] text-[20px] md:text-[24px] lg:text-[28px] mt-[40px] font-poppins'>
       How DFOS uses IIOT?
       </p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>DFOS is a DIY platform where one can create a tailor-made automation solution for their business. With this platform, you can select several sets of solutions that you find relevant for your business. Using the deep tech technologies like IIOT, AI, RPA, and iBPM, DFOS help your manufacturing unit switch to smart manufacturing. Being an intuitive UI platform, DFOS is easy to use and a highly scalable platform. So, you can create need-specific dashboards.</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins text-justify'>DFOS is a DIY platform where one can create a tailor-made automation solution for their business. With this platform, you can select several sets of solutions that you find relevant for your business. Using the deep tech technologies like IIOT, AI, RPA, and iBPM, DFOS help your manufacturing unit switch to smart manufacturing. Being an intuitive UI platform, DFOS is easy to use and a highly scalable platform. So, you can create need-specific dashboards.</p>
       <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>With our platform, you can reap the benefits of Industry 4.0 and be one step closer to becoming a completely automated shop floor. So go digital with DFOS!.</p>
 
-      <p className='text-[#FCFCFC] text-[28px] mt-[40px] font-poppins'>
+      <p className='text-[#FCFCFC] text-[20px] md:text-[24px] lg:text-[28px] mt-[40px] font-poppins'>
       The Bottom Line
       </p>
-      <p className='text-[#C8C8C8] text-[18px] font-poppins text-justify'>In today’s environment Industrial Internet of Things environment, challenges, opportunities, and directions are everywhere. But, knowing how to use it effectively for your organization is tough. Thus, to help you with it, we have created a platform – DFOS. Furthermore, the industrial internet of things can be used in many ways. Explore them today with DesignX!</p>
+      <p className='text-[#C8C8C8] text-[14px] lg:text-[18px] font-poppins text-justify'>In today’s environment Industrial Internet of Things environment, challenges, opportunities, and directions are everywhere. But, knowing how to use it effectively for your organization is tough. Thus, to help you with it, we have created a platform – DFOS. Furthermore, the industrial internet of things can be used in many ways. Explore them today with DesignX!</p>
     </div>
   )
 }
--- src/Components/Blogs/BlogsContent.js
+++ src/Components/Blogs/BlogsContent.js
@@ -6,7 +6,7 @@ const BlogsContent = () => {
   return (
     <div className='mb-[12px] blogContent mt-[180px] sm:mt-[250px] lg:mt-[300px]'>
       <div className='blogContentBackground flex flex-col lg:grid lg:grid-cols-2 w-[88%] h-auto border-[5px] border-solid border-[#0943CB] rounded-[24px] ml-[20px] pb-[20px] sm:ml-[50px] lg:ml-[96px]'>
-        <img src={blogPic} alt="" className='rounded-24px' />
+        <img src={blogPic} alt="" className='rounded-24px h-[107.5%]' />
         <div className='font-poppins mt-[0px] lg:mt-[41px] ml-[10px]'>
             <p className='text-white text-[24px] xl:text-[28px] font-medium'>Industrial Internet of Things: Meaning, Benefits and Uses</p>
             <p className='text-[14px] xl:text-[18px] text-[#adadad] font-medium'>Unveil a plethora of opportunities with IIOT solutions.</p>
