da629eeab2f672bbd450018992d28ec8cf77608b
ankitkadyan06 <ankit.kadyan@designx.in>
2024-01-15 12:06:23 +0530

    text chnage in privacy policy

--- src/Pages/PrivacyPolicy.js
+++ src/Pages/PrivacyPolicy.js
@@ -112,19 +112,19 @@ const PrivacyPolicy = () => {
       </div>
       <p className="text-[14px] sm:text-[16px] lg:text-[18px] text-[#C8C8C8] font-normal">
         You may consult this list to find the Privacy Policy for each of the
-        advertising partners of DesignX.
+        advertising partners of <span className="font-semibold">DesignX</span>.
       </p>
       <p className="text-[14px] sm:text-[16px] lg:text-[18px] text-[#C8C8C8] font-normal">
         Third-party ad servers or ad networks uses technologies like cookies,
         JavaScript, or Web Beacons that are used in their respective
-        advertisements and links that appear on DesignX, which are sent directly
+        advertisements and links that appear on <span className="font-semibold">DesignX</span>, which are sent directly
         to users’ browser. They automatically receive your IP address when this
         occurs. These technologies are used to measure the effectiveness of
         their advertising campaigns and/or to personalize the advertising
         content that you see on websites that you visit.
       </p>
       <p className="text-[14px] sm:text-[16px] lg:text-[18px] text-[#C8C8C8] font-normal">
-        Note that DesignX has no access to or control over these cookies that
+        Note that <span className="font-semibold">DesignX</span> has no access to or control over these cookies that
         are used by third-party advertisers.
       </p>
       <div className="text-[18px] lg:text-[24px] xl:text-[28px] text-white font-poppins font-medium">
