a2e43aa80f04a4bc106b45b9b92161a838eba54f
ankitkadyan06 <ankit.kadyan@designx.in>
2024-02-01 14:49:25 +0530

    fixed meta tag index.html

--- src/Components/Contact/SubmitForm.js
+++ src/Components/Contact/SubmitForm.js
@@ -7,7 +7,7 @@ import { useFormik } from "formik";
 import { formValidation } from "../../schemas/index";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 const SubmitForm = () => {
   const [loading, setLoading] = useState(false);
