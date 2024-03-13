397c3ee4cc3cf476a1a120e83766029e66bdf9e0
ankitkadyan06 <ankit.kadyan@designx.in>
2024-03-13 14:02:38 +0530

    added svg to hero newsletter section and added data layout for newsletters

---
+++ src/Components/Newsletter/NewsletterData.js
@@ -0,0 +1,130 @@
+import React from "react";
+import christmas from "../Assets/Images/christmas.svg";
+const NewsletterData = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="newNewsletter pl-[145px] pr-[31px] pt-[45px]">
+          <img src={christmas} alt="" className="w-full" />
+          <p className="text-[32px] text-white font-poppins font-medium mt-[30px]">
+            Bring the revolutionary change to shop floors 
+          </p>
+          <p className="text-[18px] text-[#D4D4D4] font-poppins font-normal">
+            Without a functional plan, managing shop floors is like solving
+            Chinese arithmetic equations. Not exaggerating, but from equipment
+            breaks to human errorse. 
+          </p>
+          <p className="text-[18px] text-[#D4D4D4] font-poppins font-normal">
+            Without a functional plan, managing shop floors is like solving
+            Chinese arithmetic equations. Not exaggerating, but from equipment
+            breaks to human errorse. 
+          </p>
+        </div>
+        <div className="pt-[40px] pl-[30px] h-[80%]">
+          <p className="text-[32px] text-white font-poppins">
+            Check out our recent newsletters
+          </p>
+          <div className="h-full overflow-y-scroll">
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p className="text-[16px] text-white font-poppins font-normal tracking-wide">
+                  Bring the revolutionary change to shop{" "}
+                </p>
+                <p className="text-[14px] text-[#D4D4D4] font-poppins font-light tracking-wide">
+                  Without a functional plan, managing shop floors is like
+                  solving Chinese arithmetic equations.{" "}
+                </p>
+                <p className="text-[12px] text-white font-poppins font-light tracking-wide">
+                  Published on:{" "}
+                  <span className="text-[#0052FF] font-normal">
+                    {" "}
+                    24-05-2023
+                  </span>
+                </p>
+              </div>
+              <div></div>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p className="text-[16px] text-white font-poppins font-normal tracking-wide">
+                  Bring the revolutionary change to shop{" "}
+                </p>
+                <p className="text-[14px] text-[#D4D4D4] font-poppins font-light tracking-wide">
+                  Without a functional plan, managing shop floors is like
+                  solving Chinese arithmetic equations.{" "}
+                </p>
+                <p className="text-[12px] text-white font-poppins font-light tracking-wide">
+                  Published on:{" "}
+                  <span className="text-[#0052FF] font-normal">
+                    {" "}
+                    24-05-2023
+                  </span>
+                </p>
+              </div>
+              <div></div>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p className="text-[16px] text-white font-poppins font-normal tracking-wide">
+                  Bring the revolutionary change to shop{" "}
+                </p>
+                <p className="text-[14px] text-[#D4D4D4] font-poppins font-light tracking-wide">
+                  Without a functional plan, managing shop floors is like
+                  solving Chinese arithmetic equations.{" "}
+                </p>
+                <p className="text-[12px] text-white font-poppins font-light tracking-wide">
+                  Published on:{" "}
+                  <span className="text-[#0052FF] font-normal">
+                    {" "}
+                    24-05-2023
+                  </span>
+                </p>
+              </div>
+              <div></div>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p className="text-[16px] text-white font-poppins font-normal tracking-wide">
+                  Bring the revolutionary change to shop{" "}
+                </p>
+                <p className="text-[14px] text-[#D4D4D4] font-poppins font-light tracking-wide">
+                  Without a functional plan, managing shop floors is like
+                  solving Chinese arithmetic equations.{" "}
+                </p>
+                <p className="text-[12px] text-white font-poppins font-light tracking-wide">
+                  Published on:{" "}
+                  <span className="text-[#0052FF] font-normal">
+                    {" "}
+                    24-05-2023
+                  </span>
+                </p>
+              </div>
+              <div></div>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p className="text-[16px] text-white font-poppins font-normal tracking-wide">
+                  Bring the revolutionary change to shop{" "}
+                </p>
+                <p className="text-[14px] text-[#D4D4D4] font-poppins font-light tracking-wide">
+                  Without a functional plan, managing shop floors is like
+                  solving Chinese arithmetic equations.{" "}
+                </p>
+                <p className="text-[12px] text-white font-poppins font-light tracking-wide">
+                  Published on:{" "}
+                  <span className="text-[#0052FF] font-normal">
+                    {" "}
+                    24-05-2023
+                  </span>
+                </p>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsletterData;
--- src/Pages/Newsletter.js
+++ src/Pages/Newsletter.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import HeaderDemo from '../Components/Common/HeaderDemo'
+import React from "react";
+import HeaderDemo from "../Components/Common/HeaderDemo";
+import SubscribeNewsletter from "../Components/Newsletter/SubscribeNewsletter";
+import NewsletterData from "../Components/Newsletter/NewsletterData";
 
 const Newsletter = () => {
   return (
-    <div className='pt-[77px]'>
-        <HeaderDemo/>
-      Newsletter
+    <div className="pt-[77px] bg-[#01071B]">
+      <HeaderDemo />
+      <SubscribeNewsletter />
+      <NewsletterData />
     </div>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
