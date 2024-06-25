fe4453ba385b00fd28bc8e02b9f2e149c7f9d024
ankitkadyan06 <ankit.kadyan@designx.in>
2024-06-25 10:51:36 +0530

    change google analytics syntax

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "keen-slider/keen-slider.min.css"
-import 'react-tooltip/dist/react-tooltip.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "keen-slider/keen-slider.min.css";
+import "react-tooltip/dist/react-tooltip.css";
+import ReactGA from "react-ga4";
 
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-VPNQ61TJKP');
+ReactGA.initialize("G-VPNQ61TJKP");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
